832f7693024876e1ecb353472e34f3ed974054e1
thiago <thiagobr@br.ibm.com>
2016-06-28 19:27:23 -0300

    Organizing the code A01

--- js/module1.js
+++ js/module1.js
@@ -8,69 +8,69 @@
 
 
 const CARS = {
-   audi:[
-     { model: 'A3', year: '2015', color:['black', 'white', 'red'] },
-     { model: 'Q5', year: '2014', color:['black', 'white', 'red'] },
-     { model: 'A1', year: '2009', color:['black', 'white', 'red'] }
-   ],
-   ferrari:[
-     { model: 'Spider', year: '2016', color:['black', 'white', 'red', 'blue'] },
-     { model: 'F50', year: '2012', color:['black', 'white', 'red', 'blue'] },
-     { model: 'F40', year: '2010', color:['black', 'white', 'red', 'blue'] }
-   ],
-   mercedes:[
-     { model: 'Kompressor', year: '2004', color:['black', 'white', 'red', 'blue'] },
-     { model: 'AMD', year: '2006', color:['black', 'white', 'red', 'blue'] },
-     { model: 'F1', year: '2016', color:['black', 'white', 'red', 'blue'] }
-   ],
-   printAudi: () => {
-     console.log("Audi Cars Models");
+  audi: [
+    { model: 'A3', year: '2015', color: ['black', 'white', 'red'] },
+    { model: 'Q5', year: '2014', color: ['black', 'white', 'red'] },
+    { model: 'A1', year: '2009', color: ['black', 'white', 'red'] }
+  ],
+  ferrari: [
+    { model: 'Spider', year: '2016', color: ['black', 'white', 'red', 'blue'] },
+    { model: 'F50', year: '2012', color: ['black', 'white', 'red', 'blue'] },
+    { model: 'F40', year: '2010', color: ['black', 'white', 'red', 'blue'] }
+  ],
+  mercedes: [
+    { model: 'Kompressor', year: '2004', color: ['black', 'white', 'red', 'blue'] },
+    { model: 'AMD', year: '2006', color: ['black', 'white', 'red', 'blue'] },
+    { model: 'F1', year: '2016', color: ['black', 'white', 'red', 'blue'] }
+  ],
+  printAudi: () => {
+    console.log("Audi Cars Models");
 
-     CARS.audi.forEach((element, index) => {
-       console.log(index + " - " +
-          " Model: " + element.model + " - " +
-          " Year: " + element.year + " - " +
-          " Colors Available: " + " [ " + element.color + " ] "
-        );
-     });
-   },
-   printFerrari: () => {
-     console.log("Ferrari Cars Models");
+    CARS.audi.forEach((element, index) => {
+      console.log(index + " - " +
+        " Model: " + element.model + " - " +
+        " Year: " + element.year + " - " +
+        " Colors Available: " + " [ " + element.color + " ] "
+      );
+    });
+  },
+  printFerrari: () => {
+    console.log("Ferrari Cars Models");
 
-     CARS.ferrari.forEach((element, index) => {
-       console.log(index + " - " +
-          " Model: " + element.model + " - " +
-          " Year: " + element.year + " - " +
-          " Colors Available: " + " [ " + element.color + " ] "
-        );
-     });
-   },
-   printMercedes: () => {
-     console.log("Mercedes Cars Models");
+    CARS.ferrari.forEach((element, index) => {
+      console.log(index + " - " +
+        " Model: " + element.model + " - " +
+        " Year: " + element.year + " - " +
+        " Colors Available: " + " [ " + element.color + " ] "
+      );
+    });
+  },
+  printMercedes: () => {
+    console.log("Mercedes Cars Models");
 
-     CARS.mercedes.forEach((element, index) => {
-       console.log(index + " - " +
-          " Model: " + element.model + " - " +
-          " Year: " + element.year + " - " +
-          " Colors Available: " + " [ " + element.color + " ] "
-        );
-     });
-   },
-   carsObject: (object) => {
-     for (const i in CARS) {
+    CARS.mercedes.forEach((element, index) => {
+      console.log(index + " - " +
+        " Model: " + element.model + " - " +
+        " Year: " + element.year + " - " +
+        " Colors Available: " + " [ " + element.color + " ] "
+      );
+    });
+  },
+  carsObject: (object) => {
+    for (const i in CARS) {
       object = console.log(CARS[i]);
-     }
-     return object;
-   },
-   init: (start) => {
-     start = [
-       CARS.printAudi(),
-       CARS.printFerrari(),
-       CARS.printMercedes(),
-       CARS.carsObject()
-     ];
-     return start;
-   }
+    }
+    return object;
+  },
+  init: (start) => {
+    start = [
+      CARS.printAudi(),
+      CARS.printFerrari(),
+      CARS.printMercedes(),
+      CARS.carsObject()
+    ];
+    return start;
+  }
 };
 
 CARS.init(this.start);
