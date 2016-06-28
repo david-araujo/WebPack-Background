5188f3823e89d46f3a63fbe6418771068c8cfada
Thiago Lima <thitheguy@gmail.com>
2016-06-27 23:00:55 -0300

    Module1 is to have ECMA Script 6 tested

--- js/module1.js
+++ js/module1.js
@@ -2,11 +2,68 @@
  *@name: module1
  *
  *@description: First module to be imported into
- *scripts.js (JQuery Library Testing)
+ *scripts.js (ECMA Script 6)
  *
  */
 
-var $ = require ('JQuery');
 
-$('h3').html('Changing content via require JQuery');
-console.log("Loaded module 1");
+const CARS = {
+   audi:[
+     { model: 'A3', year: '2015', color:['black', 'white', 'red'] },
+     { model: 'Q5', year: '2014', color:['black', 'white', 'red'] },
+     { model: 'A1', year: '2009', color:['black', 'white', 'red'] }
+   ],
+   ferrari:[
+     { model: 'Spider', year: '2016', color:['black', 'white', 'red', 'blue'] },
+     { model: 'F50', year: '2012', color:['black', 'white', 'red', 'blue'] },
+     { model: 'F40', year: '2010', color:['black', 'white', 'red', 'blue'] }
+   ],
+   mercedes:[
+     { model: 'Kompressor', year: '2004', color:['black', 'white', 'red', 'blue'] },
+     { model: 'AMD', year: '2006', color:['black', 'white', 'red', 'blue'] },
+     { model: 'F1', year: '2016', color:['black', 'white', 'red', 'blue'] }
+   ],
+   printAudi: () => {
+     console.log("Audi Cars Models");
+
+     CARS.audi.forEach((element, index) => {
+       console.log(index + " - " +
+          " Model: " + element.model + " - " +
+          " Year: " + element.year + " - " +
+          " Colors Available: " + " [ " + element.color + " ] "
+        );
+     });
+   },
+   printFerrari: () => {
+     console.log("Ferrari Cars Models");
+
+     CARS.ferrari.forEach((element, index) => {
+       console.log(index + " - " +
+          " Model: " + element.model + " - " +
+          " Year: " + element.year + " - " +
+          " Colors Available: " + " [ " + element.color + " ] "
+        );
+     });
+   },
+   printMercedes: () => {
+     console.log("Mercedes Cars Models");
+
+     CARS.mercedes.forEach((element, index) => {
+       console.log(index + " - " +
+          " Model: " + element.model + " - " +
+          " Year: " + element.year + " - " +
+          " Colors Available: " + " [ " + element.color + " ] "
+        );
+     });
+   },
+   init: (start) => {
+     start = [
+       CARS.printAudi(),
+       CARS.printFerrari(),
+       CARS.printMercedes()
+     ];
+     return start;
+   }
+};
+
+CARS.init(this.start);
