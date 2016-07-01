3b17a3dc5aef988107899aed9886aec1308259f1
Thiago Lima <thitheguy@gmail.com>
2016-07-01 20:12:14 -0300

    Merging code with remote TP

--- js/module1.js
+++ js/module1.js
@@ -34,17 +34,26 @@ const CARS = {
   /**
    * @description: Objects being looped in scope
    */
-  
+
   printAudi: () => {
     console.log("Audi Cars Models:");
 
-    CARS.audi.forEach((element, index) => {
-      console.log(index + " - " +
-        " Model: " + element.model + " - " +
-        " Year: " + element.year + " - " +
-        " Colors Available: " + " [ " + element.color + " ] "
-      );
-    });
+    try {
+
+      CARS.audi.forEach((element, index) => {
+        console.log(index + " - " +
+          " Model: " + element.model + " - " +
+          " Year: " + element.year + " - " +
+          " Colors Available: " + " [ " + element.color + " ] "
+        );
+      });
+
+    } catch (err) {
+
+      document.querySelector('.err').innerHTML = err;
+      console.log("An error accured on loop Audi Array: ", err);
+      
+    }
   },
   printFerrari: () => {
     console.log("Ferrari Cars Models:");
@@ -130,7 +139,7 @@ const CARS = {
 
   },
   mercedesFilterObject: (object) => {
-    
+
     object = CARS.mercedes.filter(car => car.year >= "2004" && car.color[0] !== "purple");
 
     console.log("Mercedes\'s Cars " + " (" + "Matching newer or equal 2004 and color black:" + ") " + " Objects:");
