1a7b6dbf84ed43907c2f6c3ea62e1e944ed48591
thiago <thiagobr@br.ibm.com>
2016-07-04 18:41:56 -0300

    Implementing ES6 environment A007

--- js/module2.js
+++ js/module2.js
@@ -1,11 +1,5 @@
-/**
- *@name: module2
- *
- *@description: First module to be imported into
- *scripts.js (Loadash)
- *
- */
+/* @Name: lodash class */
 
 const lodash = require('lodash');
 
-console.log("Lodash testing dummy data", + lodash);
+console.log('Lodash testing dummy data: ', lodash);
