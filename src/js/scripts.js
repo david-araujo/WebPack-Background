e3268d1910d3a8c7257c15280b17a72deb5cc842
Thiago Lima <thitheguy@gmail.com>
2016-07-06 23:38:02 -0300

    Appropriate settings A017

---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+/**
+ *@name: scripts.js
+ *
+ *@description: File which is responsable to require
+ *other Java Script modules in this folder.
+ *
+ */
+
+require('./module1.js');
+require('./module2.js');
+require('./module3.js');
