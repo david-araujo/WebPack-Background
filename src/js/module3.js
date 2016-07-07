6b5e3bb8c268fd9fe41527f0cc434716d51e6e3b
Thiago Lima <thitheguy@gmail.com>
2016-07-06 23:37:27 -0300

    Appropriate settings A016

---
+++ src/js/module3.js
@@ -0,0 +1,35 @@
+/** @name: module3 @description: First module to be imported into */
+
+const $ = require('JQuery');
+
+const arr = ['New York', 'Sao Paulo', 'Toquio'];
+
+const listWrap = $('<ul></ul>');
+
+const section = {
+
+  citiesWrapper: $('.carsList')
+
+};
+
+let builder = (listItem) => {
+
+  for (let i = 0; i < arr.length; i++) {
+
+    let listItem = $('<li></li>');
+
+    let textItem = $('<a></a>');
+
+    textItem.text(arr[i]);
+    listItem.append(textItem);
+    listWrap.append(listItem);
+
+  }
+
+  section.citiesWrapper.append(listWrap);
+
+};
+
+builder();
+
+$('h3').html('Changing content via require JQuery');
