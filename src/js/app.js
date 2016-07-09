76583615dcae532524d27dd2f5de41f797fe56b1
Thiago Lima <thitheguy@gmail.com>
2016-07-09 12:46:40 -0300

    Lodash Settings A001

--- src/js/app.js
+++ src/js/app.js
@@ -356,8 +356,17 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	var lodash = __webpack_require__(3);
 	
-	console.log('Lodash testing dummy data: ', lodash);
-	console.log('Testing relative path');
+	var PROGRAMMERS = {
+	
+	  /** @description: Array of Objects Collections */
+	
+	  frontend: [{ user: 'james', age: '25', languages: ['java script', 'php', 'python'] }, { user: 'robert', age: '24', languages: ['asp', 'css', 'html5'] }, { user: 'john', age: '29', languages: ['css', 'java script', 'sass'] }],
+	  backend: [{ user: 'Spider', age: '26', languages: ['php', 'nodejs', 'ruby on rails', 'asp'] }, { user: 'F50', age: '22', languages: ['nodejs', 'php', 'java'] }, { user: 'F40', age: '20', languages: ['ruby on rails', 'aspnet', 'php'] }],
+	  fullstack: [{ user: 'AMD', age: '26', languages: ['php', 'nodejs', 'ruby on rails', 'asp'] }, { user: 'Kompressor', age: '24', languages: ['nodejs', 'asp', 'java'] }, { user: 'F1', age: '26', languages: ['java', 'python', 'nodejs', 'php'] }]
+	
+	};
+	
+	console.log('Lodash testing dummy data: ', PROGRAMMERS + ' ' + lodash);
 
 /***/ },
 /* 3 */
